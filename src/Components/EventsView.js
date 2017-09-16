4a79dd7b99ff4cabef0fc9a08356b2fd169c4844
Katarzyna Sitarz <katarzyna.sitarz@gmail.com>
2017-09-16 19:30:09 +0200

    category icons (but not working yet)

--- src/Components/EventsView.js
+++ src/Components/EventsView.js
@@ -14,6 +14,16 @@ const toggleView = {
     borderRadius: '40px'
 }
 
+const buttonView = {
+    height: '30px',
+    width: '30px',
+    margin: '5px',
+    borderRadius: '40px',
+    padding: '0'
+}
+
+
+
 class EventsView extends React.Component {
     state = {
         showGrid: false
@@ -28,6 +38,35 @@ class EventsView extends React.Component {
         return (
             <div className="eventsview">
                 <h1>najbliższe imprezy</h1>
+                <div>
+                    <h2>go!&nbsp;&nbsp;<FontAwesome className="fa fa-arrow-right"/>
+                        &nbsp;&nbsp;
+                    <Button style={buttonView} bsStyle="info">
+                        <FontAwesome className="fa fa-music"/>
+                    </Button>
+                    <Button style={buttonView} bsStyle="info">
+                        <FontAwesome className="fa fa-film"/>
+                    </Button>
+                    <Button style={buttonView} bsStyle="info">
+                        <FontAwesome className="fa fa-coffee"/>
+                    </Button>
+                    <Button style={buttonView} bsStyle="info">
+                        <FontAwesome className="fa fa-cutlery"/>
+                    </Button>
+                    <Button style={buttonView} bsStyle="info">
+                        <FontAwesome className="fa fa-glass"/>
+                    </Button>
+                    <Button style={buttonView} bsStyle="info">
+                        <FontAwesome className="fa fa-futbol-o"/>
+                    </Button>
+                    <Button style={buttonView} bsStyle="info">
+                        <FontAwesome className="fa fa-gamepad"/>
+                    </Button>
+                    <Button style={buttonView} bsStyle="info">
+                        <FontAwesome className="fa fa-university"/>
+                    </Button>
+                    </h2>
+                </div>
                 <div className="importedevents">
                     <Button style={toggleView} bsStyle="info"
                     onClick={this.handleGridToggleClick}
@@ -41,9 +80,9 @@ class EventsView extends React.Component {
                             <EventsGrid events={events}/>
                             :
                             <EventsList events={events}/>
-
                     }
                 </div>
+
             </div>
         )
 
